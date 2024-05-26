638f9d37c2258773f0833786e887755e447c5749
Euraxluo <euraxluo@outlook.com>
2024-05-26 23:25:54 +0800

    remove local dep

--- sui/tokenization/publish.sh
+++ sui/tokenization/publish.sh
@@ -2,7 +2,7 @@
 
 # 执行 sui client call 命令并捕获输出
 # out=$(sui client publish --skip-dependency-verification --skip-fetch-latest-git-deps )
-out=$(sui client publish --with-unpublished-dependencies)
+out=$(sui client publish)
 
 if [ $? -eq 0 ]; then
     # 打印命令输出
